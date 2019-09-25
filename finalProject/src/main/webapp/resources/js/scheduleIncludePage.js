b86bd2e86557845389b8a07a06395e8d16a497c6
SDEDU03 <SDEDU03@DESKTOP-3JLUTMT>
2019-09-25 18:13:19 +0900

    * 채팅 창 사이즈 조정 - 스크롤바 없앰 * 챗봇 출력 내용 수정

--- finalProject/src/main/webapp/resources/js/scheduleIncludePage.js
+++ finalProject/src/main/webapp/resources/js/scheduleIncludePage.js
@@ -3,11 +3,10 @@ function addChat(){
 	var partner;
 	$('#chat').click(function(){
 		var id = $('#memberID').text();
-		alert(id);
 		window.open(
 				'http://192.168.0.39/chatting/chat.go?m_id='+id+'&s_no=1&s_part=강남길',
 				'window_name',
-				'width=400px,height=600px,location=no,status=no,scrollbars=yes,resizable=no,top=300px,left=1000px'
+				'width=400px,height=600px,directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,top=300px,left=1000px'
 		);
 	});
 }
